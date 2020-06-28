013bf43fc2875c60e0080780dfaf0764c416c202
Brandon Gomes <bhgomes.code@gmail.com>
2020-06-28 18:59:40 -0400

    update Complex

---
+++ src/complex.lean
@@ -0,0 +1,165 @@
+/- ------------------------------------------------------------------------- -\
+| @project: riemann_hypothesis                                                |
+| @file:    complex.lean                                                      |
+| @author:  Brandon H. Gomes                                                  |
+| @affil:   Rutgers University                                                |
+\- ------------------------------------------------------------------------- -/
+
+import .algebra
+
+/-!
+-/
+
+--———————————————————————————————————————————————————————————————————————————————————————--
+variables (ℂ : Type*) [has_one ℂ] [DifferenceDomain ℂ]
+
+/--
+-/
+structure Complex
+    := (real        : ℂ → ℂ)
+       (Real        : SubDomain real)
+       (int         : ℂ → ℂ)
+       (Int         : SubDomain int)
+       (abs         : ℂ → membership real)
+       (int_is_real : Π {z}, z ~ int → z ~ real)
+       (zero_is_int : 0 ~ int)
+       (one_is_int  : 1 ~ int)
+       (real_lt     : membership real → membership real → Prop)
+       (zero_lt_one : real_lt ⟨_, int_is_real zero_is_int⟩ ⟨_, int_is_real one_is_int⟩)
+
+namespace Complex --—————————————————————————————————————————————————————————————————————--
+variables {ℂ} (ℭ : Complex ℂ)
+
+/--
+-/
+def Z
+    := ℭ.Int.member
+
+/--
+-/
+def Re
+    := ℭ.Real.member
+
+/--
+-/
+def imag (z)
+    := z - ℭ.real z
+
+/--
+-/
+def Imag : SubDomain ℭ.imag :=
+{
+    idempotent :=
+        begin
+            intros,
+            repeat { rw imag },
+            rw ℭ.Real.respects_sub,
+            rw ℭ.Real.idempotent,
+            rw DifferenceDomain.sub_cancel,
+            rw DifferenceDomain.sub_right_id,
+        end,
+    respects_sub :=
+        begin
+            intros,
+            repeat { rw imag },
+            rw ℭ.Real.respects_sub,
+            rw DifferenceDomain.sub_inner_swap,
+        end
+}
+
+/--
+-/
+def Im
+    := ℭ.Imag.member
+
+/--
+-/
+instance real_has_lt : has_lt (membership ℭ.real)
+    := ⟨ℭ.real_lt⟩
+
+/--
+-/
+def int_as_real (n : membership ℭ.int) : membership ℭ.real
+    := ⟨n.elem, ℭ.int_is_real n.is_member⟩
+
+/--
+-/
+def int_lt (m n : membership ℭ.int)
+    := (ℭ.int_as_real m) < (ℭ.int_as_real n)
+
+/--
+-/
+instance int_has_lt : has_lt (membership ℭ.int)
+    := ⟨ℭ.int_lt⟩
+
+/--
+-/
+def zero_int : membership ℭ.int
+    := ⟨0, ℭ.zero_is_int⟩
+
+/--
+-/
+instance int_has_zero : has_zero (membership ℭ.int)
+    := ⟨ℭ.zero_int⟩
+
+/--
+-/
+def zero_real : membership ℭ.real
+    := ⟨0, ℭ.int_is_real ℭ.zero_is_int⟩
+
+/--
+-/
+instance real_has_zero : has_zero (membership ℭ.real)
+    := ⟨ℭ.zero_real⟩
+
+/--
+-/
+def one_int : membership ℭ.int
+    := ⟨1, ℭ.one_is_int⟩
+
+/--
+-/
+instance int_has_one : has_one (membership ℭ.int)
+    := ⟨ℭ.one_int⟩
+
+/--
+-/
+def one_real : membership ℭ.real
+    := ⟨1, ℭ.int_is_real ℭ.one_is_int⟩
+
+/--
+-/
+instance real_has_one : has_one (membership ℭ.real)
+    := ⟨ℭ.one_real⟩
+
+/--
+-/
+structure ℝpos extends membership ℭ.real
+    := (is_pos : ℭ.zero_real < to_membership)
+
+/--
+-/
+def one_real_pos : ℭ.ℝpos
+    := ⟨ℭ.one_real, ℭ.zero_lt_one⟩
+
+/--
+-/
+instance real_pos_has_one : has_one ℭ.ℝpos
+    := ⟨ℭ.one_real_pos⟩
+
+/--
+-/
+structure ℤpos extends membership ℭ.int
+    := (is_pos : ℭ.zero_int < to_membership)
+
+/--
+-/
+def one_int_pos : ℭ.ℤpos
+    := ⟨ℭ.one_int, ℭ.zero_lt_one⟩
+
+/--
+-/
+instance int_pos_has_one : has_one ℭ.ℤpos
+    := ⟨ℭ.one_int_pos⟩
+
+end Complex --———————————————————————————————————————————————————————————————————————————--
--- src/selberg.lean
+++ src/selberg.lean
@@ -1,7 +1,13 @@
--- @file:   selberg.lean
--- @author: Brandon H. Gomes
--- @affil:  Rutgers University
--- @source: arXiv:1308.3067v2 [math.NT] 13 Feb 2015
+/- ------------------------------------------------------------------------- -\
+| @project: riemann_hypothesis                                                |
+| @file:    selberg.lean                                                      |
+| @author:  Brandon H. Gomes                                                  |
+| @affil:   Rutgers University                                                |
+| @source:  arXiv:1308.3067v2 [math.NT] 13 Feb 2015                           |
+\- ------------------------------------------------------------------------- -/
+
+import .complex
+import .reduction
 
 /-!
 # Selberg Class
@@ -9,257 +15,27 @@
 ...
 -/
 
-section basic --—————————————————————————————————————————————————————————————————————————--
-
-/--
-`Power X` (`𝒫 X`) The powerset of a type.
--/
-def Power (X : Sort*)
-    := X → Sort*
-notation `𝒫` X := Power X
-
-/--
-`const b` (`↓b`) The constant function at a point.
--/
-def const {X Y : Type*}
-    := λ b : Y, (λ x : X, b)
-notation `↓`:max y:max := const y
-
-/--
--/
-instance pointwise_lt {X Y : Type*} [has_lt Y] : has_lt (X → Y)
-    := ⟨λ f g, (Π x, f x < g x)⟩
-
-/--
--/
-instance lt_has_le {Y : Type*} [has_lt Y] : has_le Y
-    := ⟨λ p q, p < q ∨ p = q⟩
-
-/--
--/
-def is_same {X : Type*} (I : X → X)
-    := λ x, I x = x
-notation x `∈` I := is_same I x
-
-/--
--/
-structure membership {X : Type*} (I : X → X)
-    := (elem      : X)
-       (is_member : elem ∈ I)
-
-end basic --—————————————————————————————————————————————————————————————————————————————--
-
-namespace membership --——————————————————————————————————————————————————————————————————--
-variables {X : Type*} (I : X → X)
-
-/- --- -/
-
-end membership --————————————————————————————————————————————————————————————————————————--
-
 section analysis --——————————————————————————————————————————————————————————————————————--
-
 /--
 -/
 def supp {X Y : Type*} [has_zero Y] (f : X → Y)
     := λ x, f x ≠ 0
-
 end analysis --——————————————————————————————————————————————————————————————————————————--
 
-section difference_domain --—————————————————————————————————————————————————————————————--
-variables (C : Type*) [has_zero C] [has_sub C] [has_mul C]
-
-/--
--/
-structure DifferenceDomain
-    := (sub_cancel       : Π z : C, z - z = 0)
-       (sub_right_id     : Π z : C, z - 0 = z)
-       (mul_right_absorb : Π z : C, z * 0 = 0)
-
-end difference_domain --—————————————————————————————————————————————————————————————————--
-
-section subdomain --—————————————————————————————————————————————————————————————————————--
-variables (C : Type*) [has_zero C] [has_sub C] [has_mul C]
-
-/--
--/
-structure SubDomain
-    := (proj         : C → C)
-       (idempotent   : Π z, proj (proj z) = proj z)
-       (respects_sub : Π x y, proj (x - y) = proj x - proj y)
-       (respects_mul : Π x y, proj (x * y) = proj x * proj y)
 
 --———————————————————————————————————————————————————————————————————————————————————————--
-variables {C} (S : SubDomain C)
+variables {ℂ : Type*} [has_one ℂ] [DifferenceDomain ℂ] (ℭ : Complex ℂ)
 
-/--
--/
-def SubDomain.membership
-    := membership S.proj
+local notation `ℝ` := membership ℭ.real
+local notation `ℤ` := membership ℭ.int
+local notation `ℝ₀` := ℭ.ℝpos
+local notation `ℤ₀` := ℭ.ℤpos
+local notation `ℜ` := ℭ.Re
+local notation `ℑ` := ℭ.Im
 
-/--
--/
-def SubDomain.member (z) : S.membership
-    := ⟨S.proj z, S.idempotent z⟩
+local notation `|` z `|` := ℭ.abs z
 
-end subdomain --—————————————————————————————————————————————————————————————————————————--
-
-section complex --———————————————————————————————————————————————————————————————————————--
-variables (C : Type*) [has_zero C] [has_one C] [has_sub C] [has_mul C]
-
-/--
--/
-structure Complex extends DifferenceDomain C
-    := (Real            : SubDomain C)
-       (Int             : SubDomain C)
-       (abs             : C → membership Real.proj)
-       (int_is_real     : Π {z}, z ∈ Int.proj → z ∈ Real.proj)
-       (zero_is_int     : (0 : C) ∈ Int.proj)
-       (one_is_int      : (1 : C) ∈ Int.proj)
-       (real_lt         : membership Real.proj → membership Real.proj → Prop)
-       (zero_lt_one     : real_lt ⟨_, int_is_real zero_is_int⟩ ⟨_, int_is_real one_is_int⟩)
-
---———————————————————————————————————————————————————————————————————————————————————————--
-variables {C} (ℂ : Complex C)
-
-/--
--/
-def Complex.int
-    := ℂ.Int.proj
-
-/--
--/
-def Complex.Z
-    := ℂ.Int.member
-
-/--
--/
-def Complex.real
-    := ℂ.Real.proj
-
-/--
--/
-def Complex.Re
-    := ℂ.Real.member
-
-/--
--/
-def Complex.Imag : SubDomain C
-    := { proj := λ z, z - ℂ.real z,
-         idempotent := sorry,
-         respects_sub := sorry,
-         respects_mul := sorry, }
-
-/--
--/
-def Complex.imag
-    := ℂ.Imag.proj
-
-/--
--/
-def Complex.Im
-    := ℂ.Imag.member
-
-/--
--/
-instance Complex.real_has_lt : has_lt ℂ.Real.membership
-    := ⟨ℂ.real_lt⟩
-
-/--
--/
-def Complex.int_lt (m n : ℂ.Int.membership)
-    := ℂ.real_lt ⟨m.elem, ℂ.int_is_real m.is_member⟩
-                 ⟨n.elem, ℂ.int_is_real n.is_member⟩
-
-/--
--/
-instance Complex.int_has_lt : has_lt ℂ.Int.membership
-    := ⟨ℂ.int_lt⟩
-
-/--
--/
-def Complex.zero_int : ℂ.Int.membership
-    := ⟨0, ℂ.zero_is_int⟩
-
-/--
--/
-instance Complex.int_has_zero : has_zero ℂ.Int.membership
-    := ⟨ℂ.zero_int⟩
-
-/--
--/
-def Complex.zero_real : ℂ.Real.membership
-    := ⟨0, ℂ.int_is_real ℂ.zero_is_int⟩
-
-/--
--/
-instance Complex.real_has_zero : has_zero ℂ.Real.membership
-    := ⟨ℂ.zero_real⟩
-
-/--
--/
-def Complex.one_int : ℂ.Int.membership
-    := ⟨1, ℂ.one_is_int⟩
-
-/--
--/
-instance Complex.int_has_one : has_one ℂ.Int.membership
-    := ⟨ℂ.one_int⟩
-
-/--
--/
-def Complex.one_real : membership ℂ.real
-    := ⟨1, ℂ.int_is_real ℂ.one_is_int⟩
-
-/--
--/
-instance Complex.real_has_one : has_one ℂ.Real.membership
-    := ⟨ℂ.one_real⟩
-
-/--
--/
-structure Complex.ℝpos extends membership ℂ.real
-    := (is_pos : ℂ.real_lt ℂ.zero_real to_membership)
-
-/--
--/
-def Complex.one_real_pos : ℂ.ℝpos
-    := ⟨ℂ.one_real, ℂ.zero_lt_one⟩
-
-/--
--/
-instance Complex.real_pos_has_one : has_one ℂ.ℝpos
-    := ⟨ℂ.one_real_pos⟩
-
-/--
--/
-structure Complex.ℤpos extends membership ℂ.int
-    := (is_pos : ℂ.int_lt ℂ.zero_int to_membership)
-
-/--
--/
-def Complex.one_int_pos : ℂ.ℤpos
-    := ⟨ℂ.one_int, ℂ.zero_lt_one⟩
-
-/--
--/
-instance Complex.int_pos_has_one : has_one ℂ.ℤpos
-    := ⟨ℂ.one_int_pos⟩
-
-end complex --———————————————————————————————————————————————————————————————————————————--
-
-section LDatum --————————————————————————————————————————————————————————————————————————--
-
-variables {C : Type*} [has_zero C] [has_one C] [has_sub C] [has_mul C]
-          (ℂ : Complex C)
-
-local notation `ℝ` := ℂ.Real.membership
-local notation `ℝ₀` := ℂ.ℝpos
-local notation `ℤ` := ℂ.Int.membership
-local notation `ℤ₀` := ℂ.ℤpos
-local notation `ℜ` := ℂ.Re
-local notation `ℑ` := ℂ.Im
-
-local notation `|` z `|` := ℂ.abs z
+namespace LDatum --——————————————————————————————————————————————————————————————————————--
 
 /--
 A1:
@@ -267,9 +43,9 @@ A1:
 - ∀k > 0, f(n) * log^k n <<_k 1
 - ∀ε > 0, ∑_{n ≤ x}|f(n)|^2 <<_ε x^ε
 -/
-structure Axiom1 (f : ℤ₀ → C)
-    := (bounded : (ℤ₀ → C) → (C → C) → Type*)
-       (real_at_one : f 1 ∈ ℂ.real)
+structure Axiom1 (f : ℤ₀ → ℂ)
+    := (bounded     : (ℤ₀ → ℂ) → (ℂ → ℂ) → Type*)
+       (real_at_one : f 1 ~ ℭ.real)
        (log_bound   : bounded f ↓1)
        (sum_bound   : empty)
 
@@ -278,11 +54,11 @@ A2:
 - x*K(x) extends to a Schwartz function on ℝ
 - lim_{x→0^+}(x * K(x)) ∈ ℝ
 -/
-structure Axiom2 (K : ℝ₀ → C)
-    := (extends_to_schwartz : (ℝ₀ → C) → 𝒫 C → Type*)
-       (lim0 : (ℝ₀ → C) → C)
-       (schwartz_extension : extends_to_schwartz (λ x, x.elem * K x) (λ x, x ∈ ℂ.real))
-       (real_limit         : lim0 (λ x, x.elem * K x) ∈ ℂ.real)
+structure Axiom2 (K : ℝ₀ → ℂ)
+    := (extends_to_schwartz : (ℝ₀ → ℂ) → 𝒫 ℂ → Type*)
+       (lim0                : (ℝ₀ → ℂ) → ℂ)
+       (schwartz_extension  : extends_to_schwartz (λ x, x.elem * K x) (λ x, x ~ ℭ.real))
+       (real_limit          : lim0 (λ x, x.elem * K x) ~ ℭ.real)
 
 /--
 A3:
@@ -291,12 +67,13 @@ A3:
 - ∑_{z ∈ supp(m), |ℜ(z)| ≤ T} |m(z)| << 1 + T^A for some A ≥ 0
 - #{z ∈ supp(m) | m(z) ∉ ℤ} < ∞
 -/
-structure Axiom3 (m : C → ℝ)
-    := (is_discrete : 𝒫 C → Type*) (has_bounded_count : 𝒫 C → Type*)
+structure Axiom3 (m : ℂ → ℝ)
+    := (is_discrete            : 𝒫 ℂ → Type*)
+       (has_bounded_count      : 𝒫 ℂ → Type*)
        (discrete_support       : is_discrete (supp m))
-       -- (horizontal_support     : ∃ y ≥ 0, (Π z, supp m z → |(ℑ z).elem| ≤ y))
+       (horizontal_support     : ∃ y ≥ 0, (Π z, supp m z → |(ℑ z).elem| ≤ y))
        (support_sum_bound      : empty)
-       (finite_non_int_support : has_bounded_count (λ z, supp m z ∧ ¬((m z).elem ∈ ℂ.int)))
+       (finite_non_int_support : has_bounded_count (λ z, supp m z ∧ ¬((m z).elem ~ ℭ.int)))
 
 /--
 A4:
@@ -304,52 +81,57 @@ A4:
     satisfying h(ℝ) ⊆ ℝ, we have the equality:
         ∑_{z∈supp(m)}m(z)h(z) = 2*ℜ( ∫_0^∞ K(x)(g(0) - g(x))dx - ∑_{n=1}^∞ f(n)g(log n) )
 -/
-structure Axiom4 (f : ℤ₀ → C) (K : ℝ₀ → C) (m : C → ℝ)
-    := (is_smooth : (ℝ → C) → Type*)
-       (is_compact : Π {X : Type*}, 𝒫 X → Type*)
-       (fourier_transform : (ℝ → C) → (C → C))
-       (special_equality : Type*)
-       (fourier_equality : Π (g) {_ : is_smooth g} {_ : is_compact (supp g)}, special_equality)
+structure Axiom4 (f : ℤ₀ → ℂ) (K : ℝ₀ → ℂ) (m : ℂ → ℝ)
+    := (is_smooth         : (ℝ → ℂ) → Type*)
+       (is_compact        : Π {X : Type*}, 𝒫 X → Type*)
+       (fourier_transform : (ℝ → ℂ) → (ℂ → ℂ))
+       (special_equality  : Type*)
+       (fourier_equality  : Π (g) {_ : is_smooth g} {_ : is_compact (supp g)}, special_equality)
+
+end LDatum --————————————————————————————————————————————————————————————————————————————--
 
 /--
 -/
 structure LDatum
-    := (f : ℤ₀ → C)
-       (K : ℝ₀ → C)
-       (m : C → ℝ)
-       (A1 : Axiom1 ℂ f)
-       (A2 : Axiom2 ℂ K)
-       (A3 : Axiom3 ℂ m)
-       (A4 : Axiom4 ℂ f K m)
+    := (f  : ℤ₀ → ℂ)
+       (K  : ℝ₀ → ℂ)
+       (m  : ℂ → ℝ)
+       (a1 : LDatum.Axiom1 ℭ f)
+       (a2 : LDatum.Axiom2 ℭ K)
+       (a3 : LDatum.Axiom3 ℭ m)
+       (a4 : LDatum.Axiom4 ℭ f K m)
+
+namespace LDatum --——————————————————————————————————————————————————————————————————————--
+variables (L : LDatum ℭ)
 
 /--
 L-Function
 - L_F(s) := ∑_{n=1}^∞ a_F(n) n^-s = exp(∑_{n=2}^∞ f(n) / log(n) * n ^ {1/2 - s}) for ℜ(s) > 1
 -/
-def LDatum.L (L : LDatum ℂ)
-    (exp : C → C) (sum : (ℤ₀ → C) → C) (inv_log : ℤ₀ → C) (pow : ℤ₀ → C → C) (inv_two : C)
-    (s : C) (σ1 : ℜ s > 1)
+def L
+    (exp : ℂ → ℂ) (sum : (ℤ₀ → ℂ) → ℂ) (inv_log : ℤ₀ → ℂ) (pow : ℤ₀ → ℂ → ℂ) (inv_two : ℂ)
+    (s : ℂ) (σ1 : ℜ s > 1)
     := exp (sum (λ n, L.f n * inv_log n * pow n (inv_two - s))) -- sum starting from n = 2 ...?
 
 /--
 Degree of F
 - d_F := 2 * lim_{x → 0^+} xK(x)
 -/
-def LDatum.degree (L : LDatum ℂ) (two : C) (lim0 : (ℝ₀ → C) → C)
+def degree (two : ℂ) (lim0 : (ℝ₀ → ℂ) → ℂ)
     := two * lim0 (λ x, x.elem * L.K x)
 
 /--
-Analytic Conductor of F
+Analytic ℂonductor of F
 - Q_F := e^(-2f(1))
 -/
-def LDatum.conductor (L : LDatum ℂ) (exp : C → C) (minus_two : C)
+def conductor (exp : ℂ → ℂ) (minus_two : ℂ)
     := exp (minus_two * L.f 1)
 
 /--
 F is positive if
 - there are at most finitely many z ∈ ℂ with m(z) < 0
 -/
-def LDatum.is_positive (L : LDatum ℂ) (finitely_many_negative : (C → ℝ) → Type*)
+def is_positive (finitely_many_negative : (ℂ → ℝ) → Type*)
     := finitely_many_negative L.m
 
 end LDatum --————————————————————————————————————————————————————————————————————————————--
