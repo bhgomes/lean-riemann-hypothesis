989c4c7fec90edf66f7b95541bb90da04bb9936f
Brandon Gomes <bhgomes.code@gmail.com>
2020-07-29 15:22:10 -0400

    add riemann hypothesis

---
+++ src/riemann_hypothesis.lean
@@ -0,0 +1,24 @@
+/- ------------------------------------------------------------------------- -|
+| @project: riemann_hypothesis                                                |
+| @file:    riemann_hypothesis.lean                                           |
+| @author:  Brandon H. Gomes                                                  |
+| @affil:   Rutgers University                                                |
+|- ------------------------------------------------------------------------- -/
+
+import .dirichlet_eta
+
+/-!
+-/
+
+--———————————————————————————————————————————————————————————————————————————————————————--
+variables {ℂ : Type*} {ℝ : Type*}
+          [has_lift_t nat ℝ] [has_lift_t ℝ ℂ] [preorder ℝ] [Algebra ℝ] [Algebra ℂ]
+          (ℭ : Complex ℂ ℝ)
+
+/--
+-/
+def riemann_hypothesis
+    [has_lift_zero_same ℕ ℝ]
+    [has_lift_lt_comm ℕ ℝ]
+    (s) (σpos : 0 < ℭ.real_part s)
+    := is_convergent ℭ.abs (dirichlet_eta.partial_pairs ℭ s) 0 → ℭ.real_part s = 2⁻¹
